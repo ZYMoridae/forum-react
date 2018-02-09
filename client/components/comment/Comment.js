870dbe3ccda06c5214be808dda16986f468f410a
Joe <ashzhouyue@gmail.com>
2018-02-09 10:33:54 +1030

    tweak

--- client/components/comment/Comment.js
+++ client/components/comment/Comment.js
@@ -1,17 +1,59 @@
 import React, { Component } from 'react';
 import renderHTML from 'react-render-html';
 import './Comment.css';
+import { Comment, Icon } from 'semantic-ui-react';
 
-export default class Comment extends Component {
+export default class CommentItem extends Component {
   render() {
+    const { comment } = this.props;
+    let nestedComments = '';
+    if(comment.replied_comments.length > 0) {
+      nestedComments =  <Comment.Group size='large'>
+                          {comment.replied_comments.map(nestedComment =>
+                              <Comment key={nestedComment.id}>
+                                <Comment.Avatar src={nestedComment.user.image} />
+                                <Comment.Content>
+                                  <Comment.Author>{nestedComment.user.username}</Comment.Author>
+                                  <Comment.Text>
+                                    { renderHTML(nestedComment.html_body) }
+                                  </Comment.Text>
+                                  <Comment.Actions>
+                                    <Comment.Action>
+                                      <Icon name='like' /> Like
+                                    </Comment.Action>
+                                    <Comment.Action>
+                                      <Icon name='reply' /> Reply
+                                    </Comment.Action>
+                                  </Comment.Actions>
+                                </Comment.Content>
+                              </Comment>
+                            )}
+                        </Comment.Group>
+    }
     return (
-      <div className="Comment">
-        <img className="Comment-user-img" src={this.props.comment.user.image} />
-        <span className="Comment-user-name">{this.props.comment.user.username}</span>
-        <div className="Comment-body">
-          { renderHTML(this.props.comment.html_body) }
-        </div>
-      </div>
+      <Comment.Group size='large' threaded>
+        <Comment>
+          <Comment.Avatar src={comment.user.image} />
+          <Comment.Content>
+            <Comment.Author>{comment.user.username}</Comment.Author>
+            <Comment.Metadata>
+              <span>{comment.created_at}</span>
+            </Comment.Metadata>
+            <Comment.Text>
+              { renderHTML(comment.html_body) }
+            </Comment.Text>
+            <Comment.Actions>
+              <Comment.Action>
+                <Icon name='like' /> Like
+              </Comment.Action>
+              <Comment.Action>
+                <Icon name='reply' /> Reply
+              </Comment.Action>
+            </Comment.Actions>
+          </Comment.Content>
+          {nestedComments}
+        </Comment>
+      </Comment.Group>
     )
   }
 }
